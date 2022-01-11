2d9bed980ba0c792cc729eeb1168360a147d3895
aschemily <aschemily@gmail.com>
2022-01-11 10:10:01 -0500

    initial

---
+++ process.py
@@ -0,0 +1,16 @@
+log_file = open("um-server-01.txt")
+
+
+def sales_reports(log_file):
+    #looping through the um-server-01.txt file
+    for line in log_file:
+        #getting rid of white spaces at end of string
+        line = line.rstrip()
+        #slicing from position 0 to 3 not included
+        day = line[0:3]
+        #finding day that's equal to tuesday and if so print line
+        if day == "Tue":
+            print(line)
+
+
+sales_reports(log_file)
